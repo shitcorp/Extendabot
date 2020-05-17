1137bbb99989d876230cb0607699900cd5b45622
MeerBiene <b.buhles@gmail.com>
2020-05-17 06:14:00 +0200

    nothing

---
+++ config.exmaple.js
@@ -0,0 +1,9 @@
+// will fill this later when i need it
+
+config = {
+
+    "token": "abcd"
+
+}
+
+exports.config;
--- src/plugins/foo-bar/index.js
+++ src/plugins/foo-bar/index.js
@@ -3,4 +3,5 @@ const config = require('./config.json')
 exports.plugin = {
     name: "Foo-Bar Plugin",
     desc: "returns bar when do say foo."
+    
 }

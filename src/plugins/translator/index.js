631cf9966fc231fae26f25a15162cc5b91c771dd
MeerBiene <b.buhles@gmail.com>
2020-05-19 10:09:25 +0100

    fixed translator plugin

--- src/plugins/translator/index.js
+++ src/plugins/translator/index.js
@@ -15,40 +15,23 @@ exports.plugin = {
             },
             help: {
                 name: "translate",
-                category: "Utilities",
+                category: "Utility",
                 description: "Translate from one language to another.",
                 usage: "translate <from> <to> <text>"
-            }
-        },
-        bye: {
-            run: async (client, message, args, level) => {
-                bye(message);
-            },
-            conf: {
-                enabled: true,
-                guildOnly: true,
-                aliases: ['goodbye'],
-                permLevel: "STAFF"
-            },
-            help: {
-                name: "bye",
-                category: "Mod",
-                description: "This is a creative text, dont mind me",
-                usage: "bye -> tells the world goodbye from you."
             }
         }
     },
     conf: {
         version: "1.0",  // BOT version
-        perms: ['KICK_MEMBERS', 'BAN_MEMBERS'],  // perms the bot needs to execute this plugin
+        perms: ['READ_MESSAGES', 'SEND_MESSAGES'],  // perms the bot needs to execute this plugin
         repo: "http://github.com", // to display if error happens
         author: "MeerBiene#7060",
         authordiscordid: "686669011601326281"
     },
     help: {
         // This info will be displayed in the global pluginmanager command
-        name: "Pluginname",
-        desc: "This is a very creative plugindescription, cause i want to see if the formatting worked properly.",
-        category: "test"
+        name: "Translator",
+        desc: "This plugin allows you/your members to easily translate something from one language to another, using the googe translator API.",
+        category: "Utility"
     }
 }
